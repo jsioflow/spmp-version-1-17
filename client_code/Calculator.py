846f61f4e12ad88a906d3c5b473335275341ce66
stone.john@mac.com <stone.john@mac.com>
2024-01-31 15:59:46 +0000

    Edited Calculator

--- client_code/Calculator.py
+++ client_code/Calculator.py
@@ -18,11 +18,11 @@ class Calculator(CalculatorTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    anvil.users.login_with_form()
-    print(f"This user has logged in: {anvil.users.get_user()['email']}")
-
+    
  # Any code you write here will run when the form opens.
     anvil.server.call('say_hello', 'Anvil Developer')
+    anvil.users.login_with_form()
+    print(f"This user has logged in: {anvil.users.get_user()['email']}")
 
   def file_loader_1_change(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
