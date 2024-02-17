1f277fcf429d1fca929c3c0faafcb7a02b8c14c7
stone.john@mac.com <stone.john@mac.com>
2024-02-17 13:18:22 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,6 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from . import Global
 
 class Form1(Form1Template):
   def __init__(self, **properties):
